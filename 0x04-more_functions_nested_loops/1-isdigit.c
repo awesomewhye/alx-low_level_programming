c8541abe52e62732d669e6d3d7d220bc3f8684a9
awesomewhye <ayormydeh@gmail.com>
2023-06-22 14:11:13 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,11 @@
   *
   * @c: it is a char on the ASCII table.
   *
-  * Returns: 1 if c is a digit, 0 otherwise.
+  * Return: 1 if c is a digit, 0 otherwise.
   */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
