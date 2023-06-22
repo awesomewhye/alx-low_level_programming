cbbbf9cff784fb721c0f7e9442d06c204effd296
awesomewhye <ayormydeh@gmail.com>
2023-06-21 17:14:03 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			j += i;
+	}
+	printf("%d\n", j);
+
+	return (0);
+}
