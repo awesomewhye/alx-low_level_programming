12f6d17b359174a6993f43ff095608481963c544
awesomewhye <ayormydeh@gmail.com>
2023-06-22 17:39:31 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point.
+  * description - Write a program that prints the numbers from 1 to 100
+  * followed by a new line.
+  * Return: 0
+  */
+int main(void)
+{
+	int test;
+
+	for (test = 1; test <= 100; test++)
+	{
+		if ((test % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((test % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((test % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", test);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
