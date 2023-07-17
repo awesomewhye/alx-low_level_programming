43b843987bff23c7749f0faf8a29dfb6fffa1e48
awesomewhye <ayormydeh@gmail.com>
2023-07-17 15:14:12 -0700

    defining a struct data typr

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+  * struct dog - defining a new type
+  * @name: dogs name
+  * @age: dogs age
+  * @owner: owners age
+  *
+  * Description: a poppy
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
